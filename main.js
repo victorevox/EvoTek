4e44dcbb2e1879bcbfa4a9331950d185603debb5
Victor Rene <victorevox@gmail.com>
2016-03-06 18:28:12 -0600

    Añadiendo mas campos y http request

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var app = angular.module('minmax', []);
 
-app.controller('MinMaxCtrl',function($scope) {
-   $scope.forModel = {}; 
+app.controller('MinMaxCtrl',function($scope, $http) {
+   $scope.formModel = {}; 
+    
+    $scope.onSubmit = function(){
+        console.log("Hey Estás registrado");
+        console.log($scope.formModel);
+        
+        $http.post('https://minmax-server.herokuapp.com/register/',$scope.formModel).success(function() {
+            console.log(":)");
+        }).error(function() {
+                console.log(":(");
+        });
+    };
 });
